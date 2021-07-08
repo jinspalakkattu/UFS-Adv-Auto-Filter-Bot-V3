5965c7f6a16f809e8d818ef886cfd8f51b28b234
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-07-08 10:46:31 +0300

    Update channel.py

--- bot/plugins/channel.py
+++ bot/plugins/channel.py
@@ -16,25 +16,25 @@ db = Database()
 @Client.on_message(filters.command(["add"]) & filters.group, group=1)
 async def connect(bot: Bot, update):
     if update.from_user.id not in OWNER_ID:
-    buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
-        InlineKeyboardButton('Source Code 🧾', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-    ], [
-        InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-    ]]
+        buttons = [[
+            InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
+            InlineKeyboardButton('Source Code 🧾', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        ], [
+            InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        ]]
 
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text="""<b>Hey {}!!</b>
-        <i>Am Just A Advance Auto Filter Bot....😉
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text="""<b>Hey {}!!</b>
+            <i>Am Just A Advance Auto Filter Bot....😉
 
-        Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b>""".format(update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-    return
+            Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b>""".format(update.from_user.first_name),
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+        return
     
     """
     A Funtion To Handle Incoming /add Command TO Connect A Chat With Group
