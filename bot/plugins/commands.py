9f4ad7c171ad237f3f1e86059eecdbf633bee4d6
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-07-08 10:46:04 +0300

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -122,23 +122,23 @@ async def help(bot, update):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     if update.from_user.id not in OWNER_ID:
-    buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
-        InlineKeyboardButton('Source Code 🧾', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-    ], [
-        InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-    ]]
+        buttons = [[
+            InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
+            InlineKeyboardButton('Source Code 🧾', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        ], [
+            InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        ]]
 
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-            update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-    return
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.START_TEXT.format(
+                update.from_user.first_name),
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+        return
     
     await bot.send_message(
         chat_id=update.chat.id,
