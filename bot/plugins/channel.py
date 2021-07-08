25ebc3b248c64dc7b12b1edf3babb23e5608daa2
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-07-08 11:40:03 +0300

    Update channel.py

--- bot/plugins/channel.py
+++ bot/plugins/channel.py
@@ -15,22 +15,12 @@ db = Database()
 
 @Client.on_message(filters.command(["add"]) & filters.group, group=1)
 async def connect(bot: Bot, update):
-    if not update.from_user.id in OWNER_ID:
-        buttons = [[
-            InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
-            InlineKeyboardButton('Source Code 🧾', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-        ], [
-            InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
-        ]]
-
-        reply_markup = InlineKeyboardMarkup(buttons)
+    if update.from_user.id not in OWNER_ID:
         await bot.send_message(
             chat_id=update.chat.id,
             text="""<b>Hey {}!!</b>
-            <i>Am Just A Advance Auto Filter Bot....😉
-
-            Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b>""".format(update.from_user.first_name),
-            reply_markup=reply_markup,
+            <i>Am Just An Advance Auto Filter Bot....😉
+            Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b></i>""".format(update.from_user.first_name),
             parse_mode="html",
             reply_to_message_id=update.message_id
         )
