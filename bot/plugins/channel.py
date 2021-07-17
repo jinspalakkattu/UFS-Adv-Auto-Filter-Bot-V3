d25692667731909ce446c327c826495ba585c1ff
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-07-17 13:58:01 +0300

    Update channel.py

--- bot/plugins/channel.py
+++ bot/plugins/channel.py
@@ -15,16 +15,16 @@ db = Database()
 
 @Client.on_message(filters.command(["add"]) & filters.group, group=1)
 async def connect(bot: Bot, update):
-    # if update.from_user.id not in OWNER_ID:
-    #     await bot.send_message(
-    #        chat_id=update.chat.id,
-    #         text="""<b>Hey {}!!</b>
-    #         <i>Am Just An Advance Auto Filter Bot....😉
-    #         Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b></i>""".format(update.from_user.first_name),
-    #         parse_mode="html",
-    #         reply_to_message_id=update.message_id
-    #     )
-    #     return
+    if update.from_user.id not in OWNER_ID:
+        await bot.send_message(
+           chat_id=update.chat.id,
+            text="""<b>Hey {}!!</b>
+            <i>Am Just An Advance Auto Filter Bot....😉
+            Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b></i>""".format(update.from_user.first_name),
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+        return
     
     """
     A Funtion To Handle Incoming /add Command TO Connect A Chat With Group
